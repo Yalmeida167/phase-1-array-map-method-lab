822a8c7ec7f151354eb7e3b87ba69bcf6ba557ed
Yago <almeidayago167@gmail.com>
2024-04-24 20:54:47 -0400

    complete array.map method lab

--- index.js
+++ index.js
@@ -11,6 +11,56 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// let splitTutorials = tutorials.map((titles) => titles.split(" "))
+  
+// let capitalizedTutorials = splitTutorials.map(titles => titles.map(word => word.charAt(0).toUpperCase() + word.slice(1)))
+
+// let correctTutorials = capitalizedTutorials.map(titles => titles.join(" "))
+
+// console.log();
+
+const titleCased = function() {
+  let splitTutorials = tutorials.map((titles) => titles.split(" "));
+  
+  let capitalizedTutorials = splitTutorials.map(titles => titles.map(word => word.charAt(0).toUpperCase() + word.slice(1)));
+
+  let correctTutorials = capitalizedTutorials.map(titles => titles.join(" "));
+
+  return correctTutorials
 }
+
+console.log(titleCased());
+
+// const skiSchool = ["aki", "guadalupe", "lei", "aalam"];
+
+// const rollCall = [];
+// 1. for(const student of skiSchool) {
+//   rollCall.push(student + " the skier")}
+
+// 2. function studentRollCall(student) {
+//   return student + " the skier"
+// }
+// 3. const rollCall = skiSchool.map(studentRollCall)
+
+// 4. const rollCall = skiSchool.map(function (student){ 
+//   return student + " the skier"})
+
+// 5. const rollCall = skiSchool.map(student => student + " the skier")
+ 
+// const robots = [
+//   {name: "Johnny 5", modes: 5, isActivated: false},
+//   {name:"C3PO", modes: 3, isActivated: false},
+//   {name: "Sonny", modes: 2.5, isActivated: false},
+//   {name: "Baymax", modes: 1.5, isActivated: false},
+// ];
+
+// const activateRobots = robots.map((robot) => {
+//   return Object.assign({}, robot, {
+//     modes: robot.modes * 2,
+//     isActivated: true,
+//   })
+// })
+// console.log(activateRobots);
+// console.log(robots);
+// console.log(rollCall)
+// console.log(skiSchool)
